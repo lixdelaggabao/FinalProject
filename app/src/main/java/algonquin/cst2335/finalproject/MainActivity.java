791487612d0270c1d34158b6b9ae3eeedc272d40
Lixdel Aggabao <agga0041@algonquinlive.com>
2023-11-19 19:46:51 -0500

    Initial commit

---
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -0,0 +1,41 @@
+package algonquin.cst2335.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button sunriseAndSunsetLookupBtn = findViewById(R.id.sunriseAndSunsetLookupBtn);
+        Button recipeSearchBtn = findViewById(R.id.recipeSearchBtn);
+        Button dictionaryBtn = findViewById(R.id.dictionaryBtn);
+        Button deezerSongSearchBtn = findViewById(R.id.deezerSongSearchBtn);
+
+        sunriseAndSunsetLookupBtn.setOnClickListener(click -> {
+            Intent intent = new Intent(MainActivity.this, SunriseAndSunsetLookup.class);
+            startActivity(intent);
+        });
+
+        recipeSearchBtn.setOnClickListener(click -> {
+            Intent intent = new Intent(MainActivity.this, RecipeSearch.class);
+            startActivity(intent);
+        });
+
+        dictionaryBtn.setOnClickListener(click -> {
+            Intent intent = new Intent(MainActivity.this, Dictionary.class);
+            startActivity(intent);
+        });
+
+        deezerSongSearchBtn.setOnClickListener(click -> {
+            Intent intent = new Intent(MainActivity.this, DeezerSongSearch.class);
+            startActivity(intent);
+        });
+    }
+}
