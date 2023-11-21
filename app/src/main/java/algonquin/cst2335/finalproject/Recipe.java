c10bb0f38b83e02ab34a95b7b879e9f3ecb61185
Lixdel Aggabao <agga0041@algonquinlive.com>
2023-11-21 09:44:31 -0500

    Finished milestone #1 (Create GUI)

---
+++ app/src/main/java/algonquin/cst2335/finalproject/Recipe.java
@@ -0,0 +1,41 @@
+package algonquin.cst2335.finalproject;
+
+public class Recipe {
+    private int id;
+    private String title;
+    private String imageUrl;
+
+    public Recipe() {
+        this(-1, "", "");
+    }
+
+    public Recipe(int id, String title, String imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
